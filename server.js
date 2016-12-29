d69ad14bf83fc77510487f59a6c5e59a6e15aa88
Rachelle <archaurore@gmail.com>
2016-12-29 13:43:08 -0500

    set up Babel and tweeked models a little

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+require('babel-core').transform("code", options);
 
 const {DATABASE_URL, PORT} = require('./config');
 const {Class} = require('./models');
