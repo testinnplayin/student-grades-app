e45041d936be665f98c31e434358724b6d96c984
Rachelle <archaurore@gmail.com>
2017-01-14 17:27:58 -0500

    class info from get injected properly into view

--- public/scripts/student-create.js
+++ public/scripts/student-create.js
@@ -1,5 +1,63 @@
 'use strict';
 
+function drawSmallerClassPanel(data) {
+	let jsClassPanel = '.js-class-panel',
+		jsPanelHeading = '.js-panel-heading',
+		jsPanelBody = '.js-panel-body',
+		div = 'div',
+		keys = Object.keys(data),
+		lng = keys.length;
+
+	$(jsClassPanel).append('<div></div>')
+	.children(div)
+	.addClass('panel-heading')
+	.addClass('js-panel-heading')
+	.append('<h4></h4>');
+
+	$(jsPanelHeading).find('h4').text('Class');
+
+	$(jsClassPanel).append('<div></div>');
+
+	$(jsClassPanel)
+	.find('div:last-child')
+	.addClass('panel-body')
+	.addClass('js-panel-body');
+
+
+	for (let i = 1; i < lng - 1; i++ ) {
+		let key = keys[i], 
+		para = '<p><strong>' + keys[i] + ':</strong> ' + data[key] + '</p>';
+
+		$(jsPanelBody).append(para);
+	}
+}
+
+function renderSmallerClass() {
+	let contentContainer = '.js-content-container',
+		classView = '.class-view',
+		jsClassView = '.js-class-view',
+		div = 'div',
+		whichClass;
+
+	$(contentContainer)
+	.append("<div></div>")
+	.find(div)
+	.addClass('js-class-view');
+
+	$(jsClassView)
+	.append('<div></div>')
+	.children(div)
+	.addClass('panel')
+	.addClass('panel-default')
+	.addClass('class-panel')
+	.addClass('js-class-panel');
+
+	whichClass = getInfoFromUrl();
+
+	retrieveKlass(whichClass);
+
+}
+
 function retrieveKlass(classIdFromUrl) {
 	let addy = '/classes/view/class/' + classIdFromUrl;
 	$.ajax({
@@ -10,6 +68,7 @@ function retrieveKlass(classIdFromUrl) {
 	.done((data) => {
 		console.log('successful get from server');
 		console.log(data);
+		drawSmallerClassPanel(data);
 	})
 	.fail((err) => {
 		console.error('unsuccessful get from server');
@@ -17,11 +76,12 @@ function retrieveKlass(classIdFromUrl) {
 	});
 }
 
-function handleCreateStudentClick() {
+function getInfoFromUrl() {
 	let classIdFromUrl = window.location.href;
 	classIdFromUrl = classIdFromUrl.split('/');
 	classIdFromUrl = classIdFromUrl[classIdFromUrl.length - 3];
-	retrieveKlass(classIdFromUrl);
+
+	return classIdFromUrl;
 }
 
-$(document).ready(handleCreateStudentClick());
+$(document).ready(renderSmallerClass());
